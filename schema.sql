bfb7a59b5a00c5870fba05616d1f3ed2bb91fc55
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2022-11-24 18:25:55 +0200

    schema.sql alkuun, katon viel lisaa

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
 #TODO 
+
+#Oisko et voi olla 20 merkkisii käyttäjänimiä max 
+#Myös et ei saa olla tyhjiä
+#En ole vielä testannut toimiiko mut kaiken järjen mukaan pitäisi toimia
+CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR (20) UNIQUE NOT NULL CHECK (username <> ''),
+	password TEXT NOT NULL CHECK (password <> '')
+);
