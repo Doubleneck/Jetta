bf82337208085e7d3f95e10894fee3f5d83c3138
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2022-11-25 00:25:37 +0200

    Alustava tietokanta tehty

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import sqlite3
+from database import Database
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    return "Heipparallaa!"
+    database = Database()
+    database.initialize_database()
+    database.connection.execute("INSERT INTO users (username, password) VALUES ('kayttaja1', 'test')")
+    database.connection.execute("INSERT INTO users (username, password) VALUES ('kayttaja2', 'test')")
+    user = database.connection.execute("SELECT username FROM users").fetchall()
+    return f"Heipparallaa! {user}"
+
