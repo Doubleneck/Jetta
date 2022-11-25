d30a60c434b4a605e356a414125163224dc22932
Taneli Härkönen <taneliharkonen@hotmail.com>
2022-11-25 14:23:23 +0200

    Make basic Jinja2 file structure and populate
      - Make App ask for login page
      - make base.html skeleton
      - make login.html extending base.html
      - make styles_main.css
      - update .gitignore

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 from database import Database
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
     database = Database()
     database.initialize_database()
     database.connection.execute("INSERT INTO users (username, password) VALUES ('kayttaja1', 'test')")
     database.connection.execute("INSERT INTO users (username, password) VALUES ('kayttaja2', 'test')")
     user = database.connection.execute("SELECT username FROM users").fetchall()
-    return f"Heipparallaa! {user}"
+    return render_template('login.html')
 
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
