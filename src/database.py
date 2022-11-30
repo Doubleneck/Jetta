5f0eeb2d5c16d705aac5128bcd070a0b710cb106
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-11-30 12:48:01 +0200

    fix database initial

--- src/database.py
+++ src/database.py
@@ -1,8 +1,9 @@
 import sqlite3
 from db_connection import the_connection
 
+
 class Database:
-    def __init__(self, path = "test.db"):
+    def __init__(self, path="test.db"):
         self.db_type = "sqlite3_file"  # Not yet maybe later
         self.db_path = path
         self.connection = the_connection
@@ -21,11 +22,12 @@ class Database:
 
     def create_tables(self):
         cursor = self.connection.cursor()
-        create_table = "CREATE TABLE IF NOT EXISTS users(\
+        create_table_users = "CREATE TABLE IF NOT EXISTS users(\
                            user_id INTEGER PRIMARY KEY AUTOINCREMENT,\
                            username VARCHAR (35) UNIQUE NOT NULL CHECK (username <> ''),\
-                           password TEXT NOT NULL CHECK (password <> ''));\
-                        CREATE TABLE IF NOT EXISTS notes(\
+                           password TEXT NOT NULL CHECK (password <> ''));"
+
+        create_table_notes = "CREATE TABLE IF NOT EXISTS notes(\
                            notes_id SERIAL PRIMARY KEY,\
                            user_id INTEGER REFERENCES users NOT NULL,\
                            bibcategory TEXT,\
@@ -33,17 +35,20 @@ class Database:
                            title TEXT,\
                            year TEXT,\
                            doiaddress TEXT);"
-        cursor.execute(create_table)
+        cursor.execute(create_table_users)
+        cursor.execute(create_table_notes)
         self.connection.commit()
         cursor.close()
 
     def drop_tables(self):
         cursor = self.connection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS users, notes;")
+        cursor.execute("DROP TABLE IF EXISTS users;")
+        cursor.execute("DROP TABLE IF EXISTS notes;")
         self.connection.commit()
 
     def initialize_database(self):
         self.drop_tables()
         self.create_tables()
+
 
 the_database = Database()
