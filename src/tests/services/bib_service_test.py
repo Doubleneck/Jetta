1d99e43c7a5c25130bcf663153d76e340f79e5ab
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-08 15:46:51 +0200

    fix for linter bib_service & tests

--- src/tests/services/bib_service_test.py
+++ src/tests/services/bib_service_test.py
@@ -1,75 +1,76 @@
 import unittest
 
 from services.bib_service import the_bib_service
-from repositories.note_repository import Note
+from repositories.note_repository import Note 
 
 
-VALID_NOTE_AS_A_BIB_STR =  "@" + "book" + \
-                "{" + 'author1999' + "," + "\n\ttitle = {" + 'title' + "}," +\
-                "\n\tauthor = {" + 'author' + "}," + "\n\tyear = {" + 'year' + "}," + \
-                "\n\tdoi_address = {" + 'doi_address' + "}\n}" + "\n"
 
-INVALID_NOTE_WRONG_CATEGORY = Note(bib_category = 'wrong_category',bib_citekey ='author1999', \
+valid_note_as_a_bib_str =  '@' + 'book' + \
+                '{' + 'author1999' + ',' + '\n\ttitle = {' + 'title' + '},' +\
+                '\n\tauthor = {' + 'author' + '},' + '\n\tyear = {' + 'year' + '},' + \
+                '\n\tdoi_address = {' + 'doi_address' + '}\n}' + '\n'
+
+invalid_note_wrong_category = Note(bib_category = 'wrong_category',bib_citekey ='author1999', \
     author= 'author', title = 'title', year = 'year', doi_address= 'doi_address')    
 
-INVALID_NOTE_CITEKEY_EMPTY = Note(bib_category = 'wrong_category',bib_citekey ='', \
+invalid_note_citekey_empty = Note(bib_category = 'wrong_category',bib_citekey ='', \
     author= 'author', title = 'title', year = 'year', doi_address= 'doi_address')  
 
 class TestBibService(unittest.TestCase):
     def setUp(self):
         self.service = the_bib_service
-        self.VALID_NOTE = Note(bib_category = 'book',bib_citekey ='author1999', \
+        self.valid_note = Note(bib_category = 'book',bib_citekey ='author1999', \
         author= 'author', title = 'title', year = 'year', doi_address= 'doi_address')
 
     def test_bib_service_returns_a_str_with_valid_input(self):
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertTrue(type(ret)is str)
+        ret_type = type(self.service.generate_bib([self.valid_note]))
+        self.assertTrue(ret_type == str)
 
     def test_generate_bib_returns_roughly_right_bibtex_form_with_a_valid_input(self):
-        ret = self.service.generate_bib([self.VALID_NOTE])
+        ret = self.service.generate_bib([self.valid_note])
         self.assertEqual(ret[0],'@')   
         self.assertEqual(ret[-2], '}')  
         self.assertEqual(ret[-1], '\n')  
 
     def test_generate_bib_returns_correctly_when_valid_input_one_note(self):
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertEqual(ret, VALID_NOTE_AS_A_BIB_STR)     
+        ret = self.service.generate_bib([self.valid_note])
+        self.assertEqual(ret, valid_note_as_a_bib_str)     
 
     def test_generate_bib_returns_a_string_when_valid_input_all_categories(self):
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertTrue(type(ret)is str)
+        ret_type = type(self.service.generate_bib([self.valid_note]))
+        self.assertTrue(ret_type == str)
 
-        self.VALID_NOTE.bib_category = 'article'
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertTrue(type(ret)is str)
+        self.valid_note.bib_category = 'article'
+        ret_type = type(self.service.generate_bib([self.valid_note]))
+        self.assertTrue(ret_type == str)
         
-        self.VALID_NOTE.bib_category = 'phdthesis'
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertTrue(type(ret)is str)
+        self.valid_note.bib_category = 'phdthesis'
+        ret_type = type(self.service.generate_bib([self.valid_note]))
+        self.assertTrue(ret_type == str)
 
-        self.VALID_NOTE.bib_category = 'misc'
-        ret = self.service.generate_bib([self.VALID_NOTE])
-        self.assertTrue(type(ret)is str)
+        self.valid_note.bib_category = 'misc'
+        ret_type = type(self.service.generate_bib([self.valid_note]))
+        self.assertTrue(ret_type == str)
 
     def test_generate_bib_raises_value_error_if_wrong_bib_category_one_note(self):
         with self.assertRaises(ValueError):
-            self.service.generate_bib([INVALID_NOTE_WRONG_CATEGORY])
+            self.service.generate_bib([invalid_note_wrong_category])
 
     def test_generate_bib_raises_value_error_if_wrong_bib_category_in_one_note_list_of_two(self):
         with self.assertRaises(ValueError):
-            self.service.generate_bib([self.VALID_NOTE,INVALID_NOTE_WRONG_CATEGORY])        
+            self.service.generate_bib([self.valid_note,invalid_note_wrong_category])        
 
     def test_generate_bib_raises_value_error_if_citekey_empty_list_of_two(self):
         with self.assertRaises(ValueError):
-            self.service.generate_bib([self.VALID_NOTE,INVALID_NOTE_CITEKEY_EMPTY]) 
+            self.service.generate_bib([self.valid_note,invalid_note_citekey_empty]) 
        
     def test_validate_note_returns_true_if_valid_note(self):
-        self.assertTrue(self.service.validate_note(self.VALID_NOTE))
+        self.assertTrue(self.service.validate_note(self.valid_note))
 
     def test_validate_note_returns_false_if_invalid_note_wrong_category(self):
         with self.assertRaises(ValueError):
-            self.service.validate_note(INVALID_NOTE_WRONG_CATEGORY)
+            self.service.validate_note(invalid_note_wrong_category)
 
     def test_validate_note_returns_false_if_invalid_note_empty_citekey(self):
         with self.assertRaises(ValueError):
-            self.service.validate_note(INVALID_NOTE_CITEKEY_EMPTY)  
+            self.service.validate_note(invalid_note_citekey_empty)  
