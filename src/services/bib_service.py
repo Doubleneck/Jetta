b21ae34a0f4361f223601d5504de40ce7c98a6d6
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-08 14:53:56 +0200

    add validation and raiseError bib_service, add unittest for bib_service

--- src/services/bib_service.py
+++ src/services/bib_service.py
@@ -9,13 +9,23 @@ class BibService:
         Returns:
             String: a string file, which follows bibtex-format
         """
-        bibtexString = ""
-        for note in notes:
-            bibtexString += "@" + note.bib_category + \
+        if len(notes) > 0:
+            print(notes)
+            bibtexString = ""
+            for note in notes:
+                print(note)
+                self.validate_note(note)
+                bibtexString += "@" + note.bib_category + \
                 "{" + note.bib_citekey + "," + "\n\ttitle = {" + note.title + "}," +\
                 "\n\tauthor = {" + note.author + "}," + "\n\tyear = {" + note.year + "}," + \
                 "\n\tdoi_address = {" + note.doi_address + "}\n}" + "\n"
-        return bibtexString
+            return bibtexString
 
+    def validate_note(self,note):
+        valid_categories = ['book','article','phdthesis','misc']
+        if note.bib_category in valid_categories and \
+            note.bib_citekey is not '':
+            return True
+        raise ValueError("The note object is not valid for bib_tex")
 
 the_bib_service = BibService()
